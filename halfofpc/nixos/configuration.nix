c6d34b677c90863498c39317f09f976fe38567f9
halfarne <halfarne@proton.me>
2024-02-12 13:43:32 +0100

    gimp 2.10.34

--- halfofpc/nixos/configuration.nix
+++ halfofpc/nixos/configuration.nix
@@ -1,4 +1,15 @@
 { config, pkgs, lib, inputs,  ... }:
+let
+     pkgs = import (builtins.fetchGit {
+         # Descriptive name to make the store path easier to identify
+         name = "revision_with_gimp_34";
+         url = "https://github.com/NixOS/nixpkgs/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "9957cd48326fe8dbd52fdc50dd2502307f188b0d";
+     }) { ìnherit (pkgs) system; };
+
+     gimp_old = pkgs.gimp;
+in
 {
   imports =
     [
@@ -195,6 +206,8 @@
      exfatprogs
      tmux
      mesa
+
+     gimp_old
 
      #minicom
      #libusb1
